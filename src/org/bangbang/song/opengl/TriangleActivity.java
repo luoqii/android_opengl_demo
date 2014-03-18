6d24b7a99f25dcfaf309f3a149950866a9c67a0f
bangbang.S <bangbang.song@gmail.com>
2014-03-18 23:22:51 +0800

    my first triangle demo.

--- src/org/bangbang/song/opengl/TriangleActivity.java
+++ src/org/bangbang/song/opengl/TriangleActivity.java
@@ -9,6 +9,7 @@ import java.nio.ShortBuffer;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import org.bangbang.song.android.commonlib.GLUtil;
 import org.bangbang.song.android.commonlib.LogRender;
 
 import android.app.Activity;
@@ -16,14 +17,35 @@ import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class TriangleActivity extends Activity {
+    private static final String TAG = TriangleActivity.class.getSimpleName();
+    private GLSurfaceView mGLSurfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         
-        GLSurfaceView v = new GLSurfaceView(this);
-        v.setRenderer(new TriangleRender());
-        setContentView(v);
+        mGLSurfaceView = new GLSurfaceView(this);
+        mGLSurfaceView.setRenderer(new TriangleRender());
+        mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        setContentView(mGLSurfaceView);
+        
+        mGLSurfaceView.requestRender();
+        
+        mGLSurfaceView.post(new Requester(mGLSurfaceView));
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        
+        mGLSurfaceView.onResume();     
+        mGLSurfaceView.requestRender();
+    }
+     @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        mGLSurfaceView.onPause();
     }
     
     static class TriangleRender  extends LogRender  {
@@ -69,7 +91,7 @@ public class TriangleActivity extends Activity {
         };
 
         // Set scolor with red, green, blue and alpha (opacity) values
-        static float scolor[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
+        static float scolor[] = { 0.2f, 0.2f, 0.2f, 1.0f };
         
         public Triangle() {
             // initialize vertex byte buffer for shape coordinates
@@ -103,12 +125,43 @@ public class TriangleActivity extends Activity {
         
        void draw(GL10 gl) {
             gl.glEnable(GL10.GL_VERTEX_ARRAY);
+            logError(gl, "glEnable(GL10.GL_VERTEX_ARRAY)");
+            gl.glEnable(GL10.GL_FLAT);
+            logError(gl, "glEnable(GL10.GL_FLAT)");
+            
+            gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+            logError(gl, "glEnableClientState(GL10.GL_VERTEX_ARRAY)");
             gl.glVertexPointer(COORDS_PER_VERTEX, GL10.GL_FLOAT, 0, mVertexBuffer);
+            logError(gl, "glVertexPointer()");
 //            gl.glEnable(GL10.GL_COLOR_ARRAY);
-            gl.glColorPointer(1, GL10.GL_UNSIGNED_BYTE, 0, mColorBuffer);
+//            gl.glColorPointer(1, GL10.GL_UNSIGNED_BYTE, 0, mColorBuffer);
+//            logError(gl, "glColorPointer()");
 //            gl.glColor4f(0f, 1f, 0f, 1);
             gl.glDrawElements(GL10.GL_TRIANGLES, 3, GL10.GL_UNSIGNED_SHORT, mIndexrBuffer);
-//            gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
+            logError(gl, "glDrawElements()");
+            //            gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
         }
+       
+    }
+    
+    class Requester implements Runnable {
+        
+        private static final int REQUEST_RENDER_DELAY_MILLIS = 5* 1000;
+        private GLSurfaceView mGlSurfaceView;
+
+        public Requester(GLSurfaceView view){
+            mGlSurfaceView = view;
+        }
+        
+        @Override
+        public void run() {
+            mGLSurfaceView.requestRender();
+            
+            mGLSurfaceView.postDelayed(this, REQUEST_RENDER_DELAY_MILLIS);
+        }
+    }
+    
+    static void logError(GL10 gl, String methodName) {
+            GLUtil.logError(gl, TAG, methodName);
     }
 }
